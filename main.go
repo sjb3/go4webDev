3c5961dbac68b99ea0fa67dc2d48e90ef8cfc08b
Sung J Byun <sungjbyun@D-108-179-128-134.dhcp4.washington.edu>
2017-01-18 21:03:23 -0800

    for web dev: do not know I could use handlebars here w/o implementing

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "net/http"
+  "fmt"
+  "html/template"
+)
+
+type Page struct{
+  Name string
+
+}
+
+func main() {
+  templates := template.Must(template.ParseFiles("templates/index.html"))
+
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+    p := Page{Name: "Gopher"}
+
+    if name := r.FormValue("name"); name !=""{
+      p.Name = name
+    }
+    if err := templates.ExecuteTemplate(w, "index.html", p);
+    err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    fmt.Fprintf(w, "<h1>Hello HandleFunc</h1>")
+  })
+
+  fmt.Println(http.ListenAndServe(":8000", nil))
+}

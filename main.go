c7479305d2bfebf4384631fa175a2b39fff5fbf0
Sung J Byun <sungjbyun@D-173-250-189-33.dhcp4.washington.edu>
2017-01-20 19:46:28 -0800

    it works with dummy data

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
   "html/template"
   _ "github.com/mattn/go-sqlite3"
   "database/sql"
+  "encoding/json"
 )
 
 type Page struct{
@@ -13,11 +14,18 @@ type Page struct{
   DBStatus bool
 }
 
+type SearchResult struct {
+  Title string
+  Author string
+  Year string
+  Id string
+}
+
 func main() {
   templates := template.Must(template.ParseFiles("templates/index.html"))
 
   db, _ := sql.Open("sqlite3", "dev.db")
-  
+
 
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
     p := Page{Name: "Gopher"}
@@ -30,6 +38,18 @@ func main() {
     if err := templates.ExecuteTemplate(w, "index.html", p);
     err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+  })
+  http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request){
+    results := []SearchResult{
+      SearchResult{"Mody Dick", "Herman Melville", "1851", "11111"},
+      SearchResult{"Huckleberry Finn", "Mark Twain", "1884", "22222"},
+      SearchResult{"The Catcher in the Rye", "JD Salinger", "1951", "33"},
+
+    }
+    encoder := json.NewEncoder(w)
+    if err := encoder.Encode(results); err != nil{
+      http.Error(w, err.Error(), http.StatusInternalServerError)
     }
   })
 

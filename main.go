d6b997d043ee6382864090966bb3c1baba736b03
Sung J Byun <sungjbyun@D-69-91-140-112.dhcp4.washington.edu>
2017-01-22 18:14:04 -0800

    no error but still not displaying

--- main.go
+++ main.go
@@ -62,6 +62,24 @@ func main() {
     }
   })
 
+  http.HandleFunc("/books/add", func(w http.ResponseWriter, r*http.Request){
+    var book ClassifyBookResponse
+    var err error
+
+    if book, err = find(r.FormValue("id")); err != nil{
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    if err = db.Ping(); err != nil{
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    _, err = db.Exec("insert into books(pk, title, author, id, classification) value(?,?,?,?,?)",
+                      nil, book.BookData.Title, book.BookData.Author, book.BookData.ID, book.Classification.MostPopular)
+
+    if err != nil{
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+  })
+
   fmt.Println(http.ListenAndServe(":8000", nil))
 }
 
@@ -69,21 +87,56 @@ type ClassifySearchResponse struct{
   Results []SearchResult `xml:"works>work"`
 }
 
+type ClassifyBookResponse struct{
+  BookData struct{
+    Title string `xml: "title,attr"`
+    Author string `xml: "author,attr"`
+    ID string `xml:"owi,attr"`
+  } `xml:"work"`
+  Classification struct{
+    MostPopular string `xml: "sfa,attr"`
+  }`xml: "recommendations>ddC>mostPopular"`
+}
+
+func find(id string)(ClassifyBookResponse, error){
+  var c ClassifyBookResponse
+  body, err := classifyAPI("http://classify.oclc.org/classify2/Classify?&summary=true&owi=" + url.QueryEscape(id))
+
+  if err !=nil{
+    return ClassifyBookResponse{}, err
+  }
+  err = xml.Unmarshal(body, &c)
+  return c, err
+}
+
 func search(query string)([]SearchResult, error){
+
+  var c ClassifySearchResponse
+  body, err := classifyAPI("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query))
+  if err != nil{
+    return []SearchResult{}, err
+  }
+
+  err = xml.Unmarshal(body, &c)
+  return c.Results, err
+}
+
+func classifyAPI(url string)([]byte, error){
   var resp *http.Response
   var err error
 
-  if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query)); err!=nil{
-    return []SearchResult{}, err
+  // if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query)); err!=nil{
+  //   return []SearchResult{}, err
+  // }
+  if resp, err = http.Get(url); err !=nil{
+    return []byte{}, err
   }
 
   defer resp.Body.Close()
-  var body []byte
-  if body, err = ioutil.ReadAll(resp.Body); err!=nil{
-    return []SearchResult{}, err
-  }
+  // var body []byte
+  // if body, err = ; err!=nil{
+  //   return []byte{}, err
+  // }
+  return ioutil.ReadAll(resp.Body)
 
-  var c ClassifySearchResponse
-  err = xml.Unmarshal(body, &c)
-  return c.Results, err
 }
